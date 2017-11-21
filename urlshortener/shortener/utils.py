754547d1cad883dfed814b608dabdcf091ea0795
skshetry <suagatchhetri@outlook.com>
2017-11-21 18:30:17 +0545

    Checked for boundary test conditions for encoding/decoding.

--- urlshortener/shortener/utils.py
+++ urlshortener/shortener/utils.py
@@ -5,6 +5,9 @@ ALPHABETS: str = string.digits + string.ascii_letters
 alphabet_length: int = len(ALPHABETS)
 
 def encode_base62(id_dec: int) ->str:
+    if id_dec==0:
+        return ALPHABETS[0]
+
     result = ''
     while id_dec > 0:
         id_dec, mod = divmod(id_dec, alphabet_length)
@@ -12,7 +15,8 @@ def encode_base62(id_dec: int) ->str:
     return result
 
 def decode_base62(id_str: str) -> int:
-    sum_total = 0
-    for power, char in enumerate(reversed(id_str)):
-        sum_total += ALPHABETS.index(char) * pow(alphabet_length, power)
-    return sum_total
+    if id_str:
+        sum_total = 0
+        for power, char in enumerate(reversed(id_str)):
+            sum_total += ALPHABETS.index(char) * pow(alphabet_length, power)
+        return sum_total
