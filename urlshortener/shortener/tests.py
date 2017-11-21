9911466d1e8c464cf0802f92c408f1610fa42dc9
skshetry <suagatchhetri@outlook.com>
2017-11-21 18:25:20 +0545

    Boundary tests for decoding/encoding.

--- urlshortener/shortener/tests.py
+++ urlshortener/shortener/tests.py
@@ -8,5 +8,23 @@ class HelperTest(TestCase):
     def test_encode(self):
         self.assertEqual(encode_base62(76), '1e')
 
+    def test_encode_on_zero(self):
+        self.assertEqual(encode_base62(0), '0')
+
+    def test_encode_on_one(self):
+        self.assertEqual(encode_base62(1), '1')
+
+    def test_encode_on_negative_numbers_return_none(self):
+        self.assertFalse(encode_base62(-10))
+
     def test_decode(self):
         self.assertEqual(decode_base62('1e'), 76)
+
+    def test_decode_on_zero(self):
+        self.assertEqual(decode_base62('0'), 0)
+
+    def test_decode_on_one(self):
+        self.assertEqual(decode_base62('1'), 1)
+
+    def test_decode_on_sends_none_on_empty_string(self):
+        self.assertFalse(decode_base62(''))
