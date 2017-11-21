a2b29826b9c6f0a8aa4229e8d2835d61f46c8973
skshetry <suagatchhetri@outlook.com>
2017-11-21 17:23:56 +0545

    Added tests for encoding base-10 numerals to base-64

--- urlshortener/shortener/tests.py
+++ urlshortener/shortener/tests.py
@@ -1,1 +1,9 @@
+"""Tests for `urlshortener`"""
 from django.test import TestCase
+
+from .utils import encode_base62
+
+
+class HelperTest(TestCase):
+    def test_encode(self):
+        self.assertEqual(encode_base62(76), '1e')
