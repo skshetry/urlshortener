8c18cd959b5cc6d49056e8eef145fbce635df374
skshetry <suagatchhetri@outlook.com>
2017-11-21 18:36:43 +0545

    Documented code for utils functions.

--- urlshortener/shortener/utils.py
+++ urlshortener/shortener/utils.py
@@ -4,8 +4,15 @@ import string
 ALPHABETS: str = string.digits + string.ascii_letters
 alphabet_length: int = len(ALPHABETS)
 
+
 def encode_base62(id_dec: int) ->str:
-    if id_dec==0:
+    """Convert base-10 numerals to base-62 numerals.
+
+    This uses the same algorithm we use on conversion
+    of decimal to binary numbers, i.e. divide by the to-be converted radix.
+    """
+    if id_dec == 0:
+        # Return first char, no need to compute for this.
         return ALPHABETS[0]
 
     result = ''
@@ -15,6 +22,7 @@ def encode_base62(id_dec: int) ->str:
     return result
 
 def decode_base62(id_str: str) -> int:
+    """Convert base-62 numerals to base-10 numerals."""
     if id_str:
         sum_total = 0
         for power, char in enumerate(reversed(id_str)):
