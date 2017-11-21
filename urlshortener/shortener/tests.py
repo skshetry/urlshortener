7ccdc7697c415fafd709cec9d5662e3610ed3be6
skshetry <suagatchhetri@outlook.com>
2017-11-21 17:41:18 +0545

    Added test for decoding from base-62 numerals into base-10 numerals.

--- urlshortener/shortener/tests.py
+++ urlshortener/shortener/tests.py
@@ -1,9 +1,12 @@
 """Tests for `urlshortener`"""
 from django.test import TestCase
 
-from .utils import encode_base62
+from .utils import encode_base62, decode_base62
 
 
 class HelperTest(TestCase):
     def test_encode(self):
         self.assertEqual(encode_base62(76), '1e')
+
+    def test_decode(self):
+        self.assertEqual(decode_base62('1e'), 76)
