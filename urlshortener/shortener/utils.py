2f0281ca76db97786b975ac6f6913b8fbe9ddfe4
skshetry <suagatchhetri@outlook.com>
2017-11-21 17:47:31 +0545

    Implemented decoding from base-62 numerals into base-10 numerals.

--- urlshortener/shortener/utils.py
+++ urlshortener/shortener/utils.py
@@ -10,3 +10,9 @@ def encode_base62(id_dec: int) ->str:
         id_dec, mod = divmod(id_dec, alphabet_length)
         result = ALPHABETS[mod] + result
     return result
+
+def decode_base62(id_str: str) -> int:
+    sum_total = 0
+    for power, char in enumerate(reversed(id_str)):
+        sum_total += ALPHABETS.index(char) * pow(alphabet_length, power)
+    return sum_total
