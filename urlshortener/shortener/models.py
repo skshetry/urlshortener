57395f9584e76c05961b5860a686184d71422fa5
skshetry <suagatchhetri@outlook.com>
2017-11-22 08:06:33 +0545

    Implemented django model for URLs.

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -1,3 +1,21 @@
+"""Models for urlshortener."""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Urls(models.Model):
+    """Models for urls."""
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    url_title = models.TextField(default='')
+    long_url = models.URLField()
+    created_by = models.ForeignKey(
+        to=User,
+        related_name='user_links',
+        null=True, blank=True
+        )
+
+    def __str__(self) -> str:
+        """Return string representation. Mostly for Django Admin and Python Shell."""
+        return str(self.pk) + ' | ' + self.created_by.get_username()
