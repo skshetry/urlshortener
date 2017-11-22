96357af6e3ca19f906d14b252adf8f75a7612863
skshetry <suagatchhetri@outlook.com>
2017-11-23 01:05:34 +0545

    Added tests for  package.

--- urlshortener/analytics/tests.py
+++ urlshortener/analytics/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Analytics
+
+
+class AnalyticsTestCase(TestCase):
+    def setUp(self):
+        self.analytics = Analytics.objects.create()
+
+    def test_str(self):
+        self.assertEqual(self.analytics.__str__(), '1')
+
+    def test_add_one(self):
+        self.analytics.add_one_view()
+        self.assertEqual(self.analytics.views_num, 1)
+
+    def test_has_verbose_name_plural(self):
+        self.assertEqual(self.analytics._meta.verbose_name_plural, 'Analytics')
