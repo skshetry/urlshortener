cda3b07a1610e557c0fc3708ea3fe3bbde8f86e8
skshetry <suagatchhetri@outlook.com>
2017-11-25 16:05:03 +0545

    Added template.

--- urlshortener/urlshortener/urls.py
+++ urlshortener/urlshortener/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+
 from django.contrib import admin
+from django.contrib.auth import views as auth_views 
 
 import shortener
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout$', auth_views.logout, name='logout'),
     url(r'^', include('shortener.urls')),
 ]
